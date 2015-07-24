e4cb747336b271c8bbcbc27c807a3876671c58dc
David Arvelo <david@davidarvelo.com>
2015-07-24 17:29:55 -0400

    Add sinon and sinon-chai to test apparatus

--- test/babel-hook.js
+++ test/babel-hook.js
@@ -1,7 +1,12 @@
 var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+
+chai.should();
+chai.use(sinonChai);
 
 global.expect = chai.expect;
-chai.should();
+global.sinon = sinon;
 
 // put babel options here
 require('babel/register')({
