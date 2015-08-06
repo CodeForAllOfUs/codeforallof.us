a628271e96e5c56165ebfd7deac0d7b2f263acf8
David Arvelo <david@davidarvelo.com>
2015-08-06 17:52:03 -0400

    Update HTML and templates

--- js/templates/org-list-item.js
+++ js/templates/org-list-item.js
@@ -2,11 +2,11 @@ export function OrgListItem(ctx) {
     var listItem = document.createElement('div');
     var tpl =
     `
-        <h1><a href="${ctx.homepage}">${ctx.name}</a></h1>
+        <h2><a href="${ctx.homepage}">${ctx.name}</a></h2>
         <p>${ctx.description}</p>
     `;
 
-    listItem.setAttribute('id', ctx.id);
+    listItem.setAttribute('data-id', ctx.id);
     listItem.className = 'list-item org-list-item';
     listItem.innerHTML = tpl;
     return listItem;
