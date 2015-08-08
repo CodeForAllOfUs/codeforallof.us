415d9cfc798f90a12efd5ee1f9ec12d8a4f14956
David Arvelo <david@davidarvelo.com>
2015-08-08 04:54:24 -0400

    Search bar shows a clear button when typed into

--- js/views/search.js
+++ js/views/search.js
@@ -1,15 +1,39 @@
-import { $$, listen } from 'utils/dom';
+import { $$, listen, addClass, removeClass } from 'utils/dom';
 import EventEmitter from 'classes/event-emitter';
 
 class SearchView extends EventEmitter {
     constructor(opts = {}) {
         super();
         this.el = $$(opts.el)[0];
+        this.searchBox = $$('input', this.el)[0];
+        this.clearButton = $$('button', this.el)[0];
 
-        // DOM event becomes EventEmitter event
-        listen(this.el, 'keyup', evt => {
+        this.init();
+    }
+
+    init() {
+        this.addListeners();
+    }
+
+    addListeners() {
+        listen(this.searchBox, 'keyup', evt => {
+            this.setClearButton();
             this.emit('keyup', evt);
         });
+
+        listen(this.clearButton, 'click', evt => {
+            this.searchBox.value = '';
+            this.setClearButton();
+            this.emit('keyup', evt);
+        });
+    }
+
+    setClearButton() {
+        if (this.searchBox.value === '') {
+            addClass(this.clearButton, 'hidden');
+        } else {
+            removeClass(this.clearButton, 'hidden');
+        }
     }
 }
 
