8086894ed4464ef3ffe61c40991768f7d8f579a4
David Arvelo <david@davidarvelo.com>
2015-08-19 20:38:13 -0400

    Rework project again to split client/server repos

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,3 @@
-var argv = require('yargs').argv;
-var django = !argv.static;
-// determine if we're doing a build
-// and if so, bypass the livereload
-var build = argv._.length ? argv._[0] === 'build' : false;
-
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var del = require('del');
@@ -20,188 +14,206 @@ var filter = require('gulp-filter');
 // livereload and sync
 var browserSync = require('browser-sync').create();
 
-// dir vars
-var source = 'frontend';
-var dest = django ? 'static/search' : source + '/public';
-var destStylesDir = dest + '/styles';
-var destScriptsDir = dest + '/scripts';
-var destJsonDir = dest + '/data';
-var destFontsDir = dest + '/fonts';
-var destImagesDir = dest + '/images';
+function runTasks(opts, done) {
+    var source = opts.source;
+    var dest = opts.dest;
 
-// js vars
-var jsDir = source + '/js';
-var jsGlob = jsDir + '/**/*.js';
-var jsLibsFile = 'libs.js';
-var jsAppFile = 'app.js';
-var jsLibsGlob = [
-    'bower_components/loader.js/loader.js',
-    'node_modules/gulp-babel/node_modules/babel-core/browser-polyfill.js',
-    'bower_components/html5shiv/dist/html5shiv.min.js',
-    'bower_components/jquery/dist/jquery.min.js',
-    'bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-    'bower_components/levenshtein-toolbox/dist/levenshtein-toolbox.amd.min.js',
-    'bower_components/mochila/dist/mochila.amd.min.js',
-];
+    var files = {
+        js: {
+            app: 'app.js',
+            libs: 'libs.js',
+        },
+    };
 
-// json vars
-var jsonGlob = [
-    'organizations.json',
-    'projects.json',
-];
+    var dir = opts.dir || {
+        // input
+        js        : source + '/js',
+        sass      : source + '/sass',
 
-// fonts vars
-var fontsGlob = [
-    source + '/bower_components/font-awesome/fonts/**/*.*',
-];
+        // output
+        styles    : dest + '/styles',
+        scripts   : dest + '/scripts',
+        json      : dest + '/data',
+        fonts     : dest + '/fonts',
+        images    : dest + '/images',
+        html      : dest,
+    };
 
-// images vars
-var imagesGlob = source + '/images/**/*.*';
+    var glob = {
+        sass   : dir.sass + '/**/*.scss',
+        html   : opts.glob && opts.glob.html ? opts.glob.html : 'index.html',
+        images : source + '/images/**/*.*',
+        fonts  : [
+            source + '/bower_components/font-awesome/fonts/**/*.*',
+        ],
+        json   : [
+            source + '/organizations.json',
+            source + '/projects.json',
+        ],
+        js     : {
+            src  : dir.js + '/**/*.js',
+            app  : dir.scripts + '/' + files.js.app,
+            lib  : dir.scripts + '/' + files.js.libs,
+            libs : [
+                source + '/bower_components/loader.js/loader.js',
+                source + '/node_modules/gulp-babel/node_modules/babel-core/browser-polyfill.js',
+                source + '/bower_components/html5shiv/dist/html5shiv.min.js',
+                source + '/bower_components/jquery/dist/jquery.min.js',
+                source + '/bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+                source + '/bower_components/levenshtein-toolbox/dist/levenshtein-toolbox.amd.min.js',
+                source + '/bower_components/mochila/dist/mochila.amd.min.js',
+            ],
+        },
+    };
 
-// html template vars
-var indexHtml = django ? 'templates/search/index.htmldjango' : source + '/index.html';
+    // tasks
+    gulp.task('copy', ['copy:json', 'copy:fonts', 'copy:images']);
 
-// css vars
-var sassDir = source + '/sass';
-var sassGlob = sassDir + '/**/*.scss';
+    gulp.task('copy:fonts', function() {
+        return gulp.src(glob.fonts)
+        .pipe(gulp.dest(dir.fonts));
+    });
 
-// tasks
-// copy files
-gulp.task('copy', ['copy:json', 'copy:fonts', 'copy:images']);
+    gulp.task('copy:images', function() {
+        return gulp.src(glob.images)
+        .pipe(gulp.dest(dir.images));
+    });
 
-gulp.task('copy:fonts', function() {
-    return gulp.src(fontsGlob)
-        .pipe(gulp.dest(destFontsDir));
-});
+    gulp.task('copy:html', function() {
+        return gulp.src(glob.html)
+        .pipe(gulp.dest(dir.html));
+    });
 
-gulp.task('copy:images', function() {
-    return gulp.src(imagesGlob)
-        .pipe(gulp.dest(destImagesDir));
-});
+    gulp.task('copy:json', ['clean:json'], function() {
+        return gulp.src(glob.json)
+        .pipe(gulp.dest(dir.json));
+    });
 
-gulp.task('copy:html', function() {
-    return gulp.src(indexHtml)
-        .pipe(gulp.dest(dest));
-});
+    gulp.task('clean:json', function(cb) {
+        del(dir.json, cb);
+    });
 
-gulp.task('copy:json', ['clean:json'], function() {
-    return gulp.src(jsonGlob)
-        .pipe(gulp.dest(destJsonDir));
-});
+    gulp.task('js:clean', function(cb) {
+        del(dir.scripts, cb);
+    });
 
-gulp.task('clean:json', function(cb) {
-    del(destJsonDir, cb);
-});
+    gulp.task('js:clean:libs', function(cb) {
+        del(glob.js.lib, cb);
+    });
 
-gulp.task('js:clean', function(cb) {
-    del(destScriptsDir, cb);
-});
+    // vendor libraries
+    gulp.task('js:libs', ['js:clean:libs'], function() {
+        return gulp.src(glob.js.libs)
+            .pipe(concat(files.js.libs))
+            .pipe(gulp.dest(dir.scripts));
+    });
 
-gulp.task('js:clean:libs', function(cb) {
-    del(destScriptsDir + '/' + jsLibsFile, cb);
-});
+    gulp.task('js:clean:app', function(cb) {
+        del(glob.js.app, cb);
+    });
 
-gulp.task('js:clean:app', function(cb) {
-    del(destScriptsDir + '/' + jsAppFile, cb);
-});
-
-// vendor libraries
-gulp.task('js:libs', ['js:clean:libs'], function() {
-    return gulp.src(jsLibsGlob)
-        .pipe(concat(jsLibsFile))
-        .pipe(gulp.dest(destScriptsDir));
-});
-
-// app source
-gulp.task('js:app', ['js:clean:app'], function() {
-    return gulp.src(jsGlob)
+    // app source
+    gulp.task('js:app', ['js:clean:app'], function() {
+        return gulp.src(glob.js.src)
         .pipe(babel({
             modules: 'amd',
             moduleIds: true,
-            sourceRoot: __dirname + '/js',
+            sourceRoot: dir.js,
             moduleRoot: null,
             experimental: 2,
         }))
         .on('error', function (err) { console.error(err.toString()); this.emit('end'); })
-        .pipe(concat(jsAppFile))
-        .pipe(gulp.dest(destScriptsDir));
-});
+        .pipe(concat(files.js.app))
+        .pipe(gulp.dest(dir.scripts));
+    });
 
-gulp.task('css:clean', function(cb) {
-    del(destStylesDir, cb);
-});
+    gulp.task('css:clean', function(cb) {
+        del(dir.styles, cb);
+    });
 
-gulp.task('sass', ['css:clean'], function() {
-    var processors = [
-        autoprefixer({ browsers: ['last 3 versions'] })
-    ];
+    gulp.task('sass', ['css:clean'], function() {
+        var processors = [
+            autoprefixer({ browsers: ['last 3 versions'] })
+        ];
 
-    return gulp.src(sassGlob)
+        return gulp.src(glob.sass)
         // css preprocessing
         // bootstrap-sass requires a precision >= 8
         .pipe(sass({ precision: 10 }))
         .on('error', function (err) { console.error(err.toString()); this.emit('end'); })
         // css postprocessing
         .pipe(postcss(processors))
-        .pipe(gulp.dest(destStylesDir))
+        .pipe(gulp.dest(dir.styles))
         // filter css for livereload
         .pipe(filter('**/*.css'))
         .pipe(browserSync.stream());
-});
+    });
 
-gulp.task('build', ['js:libs', 'js:app', 'sass', 'copy']);
-gulp.task('watch', ['build'], function() {
-    var events =  {};
-    function throttle(task) {
-        return function(event, file) {
-            var ev, timer;
+    gulp.task('build', ['js:libs', 'js:app', 'sass', 'copy']);
+    gulp.task('watch', ['build'], function() {
+        function throttle(task) {
+            return function(event, file) {
+                var ev, timer;
 
-            if (!events[event]) {
-                events[event] = {};
-            }
+                if (!events[event]) {
+                    events[event] = {};
+                }
 
-            ev = events[event];
-            timer = ev[file];
+                ev = events[event];
+                timer = ev[file];
 
-            if (timer) {
-                clearTimeout(timer);
-            }
+                if (timer) {
+                    clearTimeout(timer);
+                }
 
-            ev[file] = setTimeout(function () {
-                gulp.start(task, function() {
-                    delete ev[file];
-                });
-            }, 500);
-        };
-    }
+                ev[file] = setTimeout(function () {
+                    gulp.start(task, function() {
+                        delete ev[file];
+                    });
+                }, 500);
+            };
+        }
 
-    var bsOpts = {
-        online: false,
-        port: 9000,
-        ui: {
-            port: 8080,
-            weinre: {
-                port: 8000,
+        var events =  {};
+        var bsOpts = {
+            online: false,
+            port: 9000,
+            ui: {
+                port: 8080,
+                weinre: {
+                    port: 8000,
+                },
             },
-        },
-    };
-
-    if (django) {
-        bsOpts.proxy = require('./config.secret.json').browserSyncProxy;
-    } else {
-        bsOpts.server = {
-            baseDir: dest,
-            index: '../index.html',
         };
-    }
 
-    browserSync.init(bsOpts);
+        if (source !== __dirname) {
+            bsOpts.proxy = opts.config.browserSyncProxy;
+        } else {
+            bsOpts.server = {
+                baseDir: dest,
+                // index.html is one level above `dest`
+                index: '../' + glob.html,
+            };
+        }
 
-    gulp.watch(jsLibsGlob, throttle(['js:libs']));
-    gulp.watch(jsGlob, throttle(['js:app']));
-    gulp.watch(sassGlob, throttle(['sass']));
-    gulp.watch(jsonGlob, throttle(['copy:json']));
-    gulp.watch(indexHtml).on('change', browserSync.reload);
-});
+        browserSync.init(bsOpts);
 
-gulp.task('default', ['watch']);
+        gulp.watch(glob.js.src, throttle(['js:app']));
+        gulp.watch(glob.sass, throttle(['sass']));
+        gulp.watch(glob.json, throttle(['copy:json']));
+        gulp.watch(glob.html).on('change', browserSync.reload);
+    });
+
+    gulp.start('watch');
+}
+
+if (process.cwd() === __dirname) {
+    gulp.task('default', function(done) {
+        runTasks({
+            source: __dirname,
+            dest: './public',
+        }, done);
+    });
+}
+
+module.exports = runTasks;
