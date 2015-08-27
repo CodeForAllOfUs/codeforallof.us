4ba35f0d8bae6a370175d99976531ab06ca33ca8
David Arvelo <david@davidarvelo.com>
2015-08-26 23:34:22 -0400

    Add client-side GitHubCache module

---
+++ js/singletons/github-cache.js
@@ -0,0 +1,56 @@
+import Mochila from 'mochila';
+import ajax from 'utils/ajax';
+
+class GitHubCache {
+    constructor() {
+        this.store = new Mochila();
+        this.store.addCollection('*');
+        this.rateLimit = {
+            resetDate: Date.now(),
+            remaining: null,
+        };
+    }
+
+    fetch(list) {
+        if (this.rateLimit.remaining === 0 && this.rateLimit.resetDate > Date.now()) {
+            return Promise.reject();
+        }
+
+        return ajax({
+            url: `api/github-data`,
+            data: list,
+        }).then(data => {
+            var array = [];
+            var dict = {};
+
+            data.github_data.forEach(datum => {
+                datum = {
+                    id: datum.github_path,
+                    data: datum.data,
+                };
+
+                dict[datum.id] = datum.data;
+                array.push(datum);
+            });
+
+            this.rateLimit.resetDate = new Date(data.rate_limit.reset_date);
+            this.rateLimit.remaining = data.rate_limit.remaining;
+            this.store.load(array);
+
+            return dict;
+        });
+    }
+
+    find(github_path) {
+        return this.store.find('*', github_path);
+    }
+}
+
+var singleton;
+
+export default function() {
+    if (!singleton) {
+        singleton = new GitHubCache();
+    }
+    return singleton;
+}
